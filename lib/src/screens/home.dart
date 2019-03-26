fda97d33465a5ef8a66ba2d9723c70c587e10c39
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-26 16:20:15 +0800

    show cat image

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
+}
